7ccac87f36628c55483065b439fcc4f68e0a511e
Evgeniy Dubskiy <edubskiy@openwaygroup.com>
2020-07-17 12:45:52 +0300

    Removed useless import

--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hacker_news/src/models/item_model.dart';
 import 'package:hacker_news/src/resources/news_api_provider.dart';
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
