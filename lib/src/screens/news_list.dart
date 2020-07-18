ccbb6d2da51fc36592681ea98c0c5fdf506f0830
Evgeniy Dubskiy <edubskiy@openwaygroup.com>
2020-07-19 01:24:27 +0300

    Added bloc and screens; added rxdart package

---
+++ lib/src/screens/news_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NewsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top news'),
+      ),
+      body: Text('i am a list'),
+    );
+  }
+}
