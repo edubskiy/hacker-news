94f6d93ca0e63e790344e651e00dfbf258009902
Evgeniy Dubskiy <edubskiy@openwaygroup.com>
2020-07-21 01:20:02 +0300

    Added news details screen and set up initial navigation between top stories and news details screen

--- lib/src/widgets/news_list_tile.dart
+++ lib/src/widgets/news_list_tile.dart
@@ -26,14 +26,14 @@ class NewsListTile extends StatelessWidget {
               return LoadingContainer();
             }
 
-            return buildTile(itemSnapshot.data);
+            return buildTile(context, itemSnapshot.data);
           },
         );
       },
     );
   }
 
-  Widget buildTile(ItemModel item) {
+  Widget buildTile(BuildContext context, ItemModel item) {
     return Column(
       children: <Widget>[
         ListTile(
@@ -45,6 +45,9 @@ class NewsListTile extends StatelessWidget {
               Text('${item.descendants}'),
             ],
           ),
+          onTap: () {
+            Navigator.pushNamed(context, '/${item.id}');
+          },
         ),
         Divider(
           height: 8.0,
