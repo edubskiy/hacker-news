72035bbf735e73afe81fe5b1ebc69baa8fe30739
Evgeniy Dubskiy <edubskiy@openwaygroup.com>
2020-07-22 00:52:13 +0300

    Replaced some html characters in comments with corresponding symbols, added loading containers skeletons for each comments loading

--- lib/src/widgets/comment.dart
+++ lib/src/widgets/comment.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hacker_news/src/models/item_model.dart';
+import 'package:hacker_news/src/widgets/loading_container.dart';
 
 class Comment extends StatelessWidget {
   final int itemId;
@@ -14,13 +15,13 @@ class Comment extends StatelessWidget {
       future: itemMap[itemId],
       builder: (context, AsyncSnapshot<ItemModel> snapshot) {
         if (!snapshot.hasData) {
-          return Text('Still loading comment');
+          return LoadingContainer();
         }
 
         final item = snapshot.data;
         final children = <Widget>[
           ListTile(
-            title: Text(item.text),
+            title: buildText(item),
             subtitle: item.by == '' ? Text('Deleted') : Text(item.by),
             contentPadding:
                 EdgeInsets.only(right: 16.0, left: (depth + 1) * 16.0),
@@ -39,5 +40,14 @@ class Comment extends StatelessWidget {
         return Column(children: children);
       },
     );
+  }
+
+  Widget buildText(ItemModel item) {
+    final text = item.text
+        .replaceAll('/&#x27;', "'")
+        .replaceAll('<p>', '\n\n')
+        .replaceAll('</p>', '');
+
+    return Text(text);
   }
 }
